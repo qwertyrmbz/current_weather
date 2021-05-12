59c5f7f142f1844453d4472a224a102976a51800
rmbziiik <77121871+rmbziiik@users.noreply.github.com>
2021-05-12 17:23:27 +0600

    Add files via upload

---
+++ currentWeather/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import requests
+import json
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    if request.method == "POST":
+        city = request.form['city']
+        country = request.form['country']
+        api_key = "2cfa343a0ad8b9990f54b9bf6c981df5"
+
+        weather_url = requests.get(
+            f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city},{country}&units=imperial')
+
+        weather_data = weather_url.json()
+
+        temp = round(weather_data['main']['temp'])
+        humidity = weather_data['main']['humidity']
+        wind_speed = weather_data['wind']['speed']
+
+        return render_template("result.html", temp=round((temp - 32) * 5/9), humidity=humidity, wind_speed=wind_speed, city=city)
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
